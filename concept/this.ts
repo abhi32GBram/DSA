2a21398004c63cf1fbcecb155c0e0a29c4bbbc8b
h6myman <abhiram.suradkar@gmail.com>
2024-02-03 23:30:41 +0530

    3rd Feb 2024

---
+++ concept/this.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+
+
+const user = {
+    name: "abhiram suradkar",
+    age: 4,
+    pluh: true
+}
+
+// Iterate and print out the keys : 
+for (key in user) {
+    console.log(key)
+}
+
+// Iterate and print out the values :
+for (key in user) {
+    console.log(user[key])
+}
+
+
